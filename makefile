66b686809a880c50b12b7b42833d9d8c52b7e033
Grigoriy Redozubov <gredozu1@binghamton.edu>
2019-03-27 08:09:19 -0400

    done with all 3 problems, now I need to time them all

--- makefile
+++ makefile
@@ -1,4 +1,12 @@
-all: program2 program3
+all: program1 program2 program3
+
+
+program1: program1.o
+	g++ program1.o -o program1
+
+program1.o: program1.cpp
+	g++ -g -c program1.cpp
+
 
 
 program2: program2.o
@@ -8,11 +16,13 @@ program2.o: program2.cpp
 	g++ -g -c program2.cpp
 
 
+
 program3: program3.o
 	g++ program3.o -o program3
 
 program3.o: program3.cpp
 	g++ -g -c program3.cpp
+
 
 
 clean:
--- program1.cpp
+++ program1.cpp
@@ -1,13 +1,82 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <string>
+
 
 using namespace std;
 
+string x; // These strings will store teh contents of filex.txt and filey.txt
+string y;
+
+vector<vector<int>> matrix; // This is the matrix that will memo
+
+
+int sub(){
+	for(int i = x.length(); i >= 0; i--){
+		for(int j = y.length(); j >= 0; j--){
+			if(i == x.length() || j == y.length()){
+				matrix[i][j] = 0;
+			}else{
+				char one = x.at(i);
+				char two = y.at(j);
+
+				 if(one == two){
+					matrix[i][j] = (matrix[i+1][j+1])+1;
+				}else{
+				matrix[i][j] = max(matrix[i+1][j], matrix[i][j+1]);
+				}
+			}
+		}
+	}
+	return matrix[0][0];
+
+}
+
+
 int main(int argc, char *argv[]){
 
+	string filex = argv[1]; // these are set to the files given in the program call
+	string filey = argv[2];
+	string output = argv[3];
+
+	ifstream xfile(filex); // opening files to read contents
+	ifstream yfile(filey);
+
+	xfile >> x; // set strings to their respective file's contents
+	yfile >> y;
 
 
+	//NOW LET'S RUN IT
+
+	int answer;
+
+	int iMax = x.length()+1;
+	int jMax = y.length()+1;
+
+	matrix.resize(iMax);  // allocate space for matrix
+	for(int i = 0; i < iMax; i++){
+		matrix[i].resize(jMax);
+//		for(int j = 0; j < jMax ; j++){
+//			matrix[i][j] = (-1);
+//		}
+
+	}
+
+
+
+
+	answer = sub();
+
+	ofstream out;
+	out.open(output);
+	out << answer;
+
+
+	xfile.close();
+	yfile.close();
+	out.close();
 
 
 
