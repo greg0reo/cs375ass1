c0c1c62aacdf18390617903fe638df141dc3bf18
Grigoriy Redozubov <gredozu1@binghamton.edu>
2019-03-27 06:46:00 -0400

    finished prog3, must time

--- makefile
+++ makefile
@@ -1,10 +1,19 @@
-all: program2
+all: program2 program3
+
 
 program2: program2.o
 	g++ program2.o -o program2
 
 program2.o: program2.cpp
 	g++ -g -c program2.cpp
+
+
+program3: program3.o
+	g++ program3.o -o program3
+
+program3.o: program3.cpp
+	g++ -g -c program3.cpp
+
 
 clean:
 	rm -f *.o
---
+++ program3.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string x; // These strings will store the contents of filex.txt and filey.txt
+string y;
+
+vector<vector<int>> matrix; // This is the matrix that will memo
+
+int sub(int xi, int yi){
+	if(xi == x.length() || yi == y.length()){
+		return 0;
+	}
+	if(matrix[xi][yi] < 0){
+		char one = x.at(xi);
+		char two = y.at(yi);
+		if(one == two){ //if two chars are equal, add 1 to LCS length
+			matrix[xi][yi] = (1 + sub(xi+1, yi+1));
+			//printf("%c", one);
+		}else{ // if two chars are unequal, LCS length remains the same as for shorter x y.
+			matrix[xi][yi] = max(sub(xi+1, yi), sub(xi, yi+1));
+		}
+	}
+	return matrix[xi][yi];
+
+}
+
+
+int main(int argc, char *argv[]){
+
+	string filex = argv[1]; //these are set to teh files given in the program call
+	string filey = argv[2];
+	string output = argv[3];
+
+	ifstream xfile(filex); //openning file to read info
+	ifstream yfile(filey);
+
+	xfile >> x; // set strings to their respective file's contents
+	yfile >> y;
+
+
+	//NOW LET'S RUN IT!
+
+	int answer;
+
+	int iMax = x.length();
+	int jMax = y.length();
+
+//	printf("No way it poops out here");
+
+	matrix.resize(iMax);
+	for(int i = 0; i< iMax; i++){ // populate the matrix with -1 (means LCS has not been found yet)
+		matrix[i].resize(jMax);
+		for(int j = 0; j < jMax ; j++){
+			matrix[i][j] = (-1);
+		}
+	}
+
+//	printf("do I make it here?");
+
+	answer = sub(0, 0);
+
+	//printf("\n%d\n", answer);
+
+	ofstream out;
+	out.open(output);
+	out << answer;
+
+	xfile.close();
+	yfile.close();
+	out.close();
+
+}
